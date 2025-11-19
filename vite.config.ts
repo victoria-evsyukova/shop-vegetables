347073ff4e5b27f7c612f91bb5d008dea3fdf87b
Victoria Evsiukova <romanchuck740@gmail.com>
2025-11-19 19:19:41 +0800

    change settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 /// <reference types="vitest" />
+/// <reference types="vite/client" />
 
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: ' /shop-vegetables',
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: './vitest.setup.mjs',
-    },
+  },
 })
